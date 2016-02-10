98509a15d6a8e053019930d5341a963ba312033e
moli <moli_cool12@yahoo.com.ph>
2016-02-10 16:53:44 +0800

    update prod settings

--- config/autoload/prod.php
+++ config/autoload/prod.php
@@ -4,9 +4,9 @@ return array(
     'db' => array(
         'adapters' => array(
             'site_db' => array(
-                'dsn' => 'mysql:dbname=mysite;host=localhost',
-                'username' => 'dev',
-                'password' => 'dev123'
+                'dsn' => 'mysql:dbname=mysite;host=my_server',
+                'username' => 'my_username',
+                'password' => 'my_password'
             ),
         )
     )
